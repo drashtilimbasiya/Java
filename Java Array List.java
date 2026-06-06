9938b7d5dd0c4dc061d45fd73bd748cff5621773
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-06 22:11:56 +0530

    Java Array List
    Java Array List

---
+++ Java Array List.java
@@ -0,0 +1,20 @@
+Result Size: 492 x 538
+import java.util.ArrayList;
+​
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  } 
+}
+​
+[Volvo, BMW, Ford, Mazda]
+
+
+REMOVE ADS
+
+×

2d1803402f18ccc0fa345521c74db6d8fa1ee657
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-13 21:27:48 +0530

    Java HashSet For Loop
    Java HashSet For Loop

---
+++ Java HashSet For Loop.java
@@ -0,0 +1,17 @@
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");  // Duplicate
+    cars.add("Mazda");
+    
+    for (String i : cars) {
+      System.out.println(i);
+    }
+  }
+}

809ba7911913539979fb5132b2689dc8fcb6f32f
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-13 21:32:03 +0530

    Java HashSet  Set Interface
    Java HashSet  Set Interface

---
+++ Java HashSet  Set Interface.java
@@ -0,0 +1,13 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main { 
+  public static void main(String[] args) { 
+    Set<String> cars = new HashSet<>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  } 
+}

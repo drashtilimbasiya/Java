7013931acb8d7651abbfd14c17919498c7def58d
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-18 21:35:42 +0530

    JAVA Linked Hash Set Remove
    JAVA Linked Hash Set Remove

---
+++ JAVA Linked Hash Set Remove.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedHashSet<String> cars = new LinkedHashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    
+    cars.remove("Volvo");
+    System.out.println(cars);
+  }
+}
